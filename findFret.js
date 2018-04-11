aa8956455006a93a86e4aadd680dfa017963b004
Ashleigh Maund <ashleighmaund@gmail.com>
2018-04-11 09:54:10 +0100

    added function that returns fret location of note on all strings in standard tuning

--- findFret.js
+++ findFret.js
@@ -1,3 +1,9 @@
+function findFretsInStandardTuning(note) {
+	var frets = {}, tuning = ['e', 'a', 'd', 'g', 'b', 'e'];
+	tuning.map(e => frets[e] = findFret(e, note))
+	return frets;
+}
+
 function findFret(string, note) {
 	var allNotes = ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#'];
 	return (allNotes.slice(allNotes.indexOf(string)) + ',' + allNotes.slice(0, allNotes.indexOf(string))).split(',').indexOf(note);
